a8cfd50ec995337a10dc92b041c4e1a9dd26c8db
jamad <jamad2001@gmail.com>
2020-01-02 22:50:25 +0200

    Update jamad_helloworld_nxt.py
    yet experimental

--- jamad_helloworld_nxt.py
+++ jamad_helloworld_nxt.py
@@ -1,4 +1,19 @@
 import nxt
 
-print('done')
 
+#!/usr/bin/env python
+
+import nxt.locator
+from nxt.motor import *
+
+def spin_around(b):
+    m_left = Motor(b, PORT_B)
+    m_left.turn(100, 360)
+    m_right = Motor(b, PORT_C)
+    m_right.turn(-100, 360)
+
+b = nxt.locator.find_one_brick()
+'''
+spin_around(b)
+'''
+print('done')
